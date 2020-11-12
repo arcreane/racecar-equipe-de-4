eca1c9f7cf6452029dd6cdb727baed7e751dfb09
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-12 10:44:20 +0100

    le timer des évenements, le temps continu n'est pas affecté

--- src/com/company/breaking.java
+++ src/com/company/breaking.java
@@ -2,9 +2,9 @@ package com.company;
 import java.util.Random;
 
 public class breaking {
-    public static void IsCarBroken(){
+    public static void IsCarBroken() throws InterruptedException {
         Random rand = new Random();
-        int breaking = rand.nextInt(100);
+        int breaking = rand.nextInt(35);
         for (int n = 1; n <= 35; n++) {
             if (n == breaking) {
                 time.canPrint = false;
@@ -13,33 +13,28 @@ public class breaking {
             }
         }
     }
-    private static void WhatPartIsBroken(){
+    private static void WhatPartIsBroken() throws InterruptedException {
         Random rand = new Random();
         int Part = rand.nextInt(5);
         if(Part == 0){
             System.out.println("De la fumée sort du moteur");
-            while (true){
-            }
+            time.timer();
         }
         if(Part == 1){
             System.out.println("La roue avant gauche a un problème");
-            while (true){
-            }
+            time.timer();
         }
         if(Part == 2){
             System.out.println("La roue avant droite a un problème");
-            while (true){
-            }
+            time.timer();
         }
         if(Part == 3){
             System.out.println("La roue arrière gauche a un problème");
-            while (true){
-            }
+            time.timer();
         }
         if(Part == 4){
             System.out.println("La roue arrière droite a un problème");
-            while (true){
-            }
+            time.timer();
         }
         time.canPrint = true;
     }
--- src/com/company/time.java
+++ src/com/company/time.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class time extends Thread{
-
+    public static int n;
     public static boolean canPrint = true;
 
     public time(String name){
@@ -11,24 +11,36 @@ public class time extends Thread{
     public void Print(int n) throws InterruptedException {
         if (canPrint){
             System.out.println(n);
-            Thread.sleep(1000);
+            sleep(1000);
             for(int m  = 0; m<10; m++){
                 System.out.println(" ");
             }
         }
     }
+
+    public static void timer() throws InterruptedException {
+        for (int m = 10;m>0;m--){
+            System.out.println(m);
+            n++;
+            sleep(1000);
+        }
+    }
     public void run(){
-        int n = 0;
+        n = 0;
         while (true){
+            n++;
             try {
                 Print(n);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             if (n == 5){
-                breaking.IsCarBroken();
+                try {
+                    breaking.IsCarBroken();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            n++;
         }
     }
 }
