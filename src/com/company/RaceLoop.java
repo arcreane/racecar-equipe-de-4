3aa04ae95846fbb47394889bcb583d89e24e959a
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-13 11:17:32 +0100

    Course fonctionnelle, il manque le boost et il faut tout mettre en classe

--- src/com/company/RaceLoop.java
+++ src/com/company/RaceLoop.java
@@ -1,24 +1,31 @@
 package com.company;
 
 public class RaceLoop {
-    public static int RaceDistance = 20000;
-    public static boolean FastCar;
-    private static int vitesse;
+    public static int RaceDistance = 10000;
+    public static int nombreTours = 0;
+    public static int vitesse;
 
     public static void init(){
-        if (FastCar = true){
-            vitesse = 100;
+        if (menu.Car == "FAST"){
+            vitesse = 28;
         }
-        else{
-            vitesse = 50;
+        else if (menu.Car == "SLOW"){
+            vitesse = 14;
         }
     }
     public static void Race() throws InterruptedException {
-        if (FastCar){
+        if (menu.Car == "FAST"){
             if(time.n%3 == 2){
                 breaking.IsCarBroken();
             }
         }
         RaceDistance = RaceDistance - vitesse;
+        if (RaceDistance <= 0){
+            RaceDistance = 10000;
+            nombreTours++;
+        }
+        if (nombreTours == 5){
+            Main.end();
+        }
     }
 }
--- src/com/company/time.java
+++ src/com/company/time.java
@@ -21,7 +21,8 @@ public class time extends Thread{
                 n = n-60;
             }
             System.out.println(m + " minute(s)"+" et "+ n + " seconde(s)");
-            System.out.println(RaceLoop.RaceDistance);
+            System.out.println(RaceLoop.RaceDistance + " mètres");
+            System.out.println("Tour numéro : " + RaceLoop.nombreTours + "/5");
             sleep(1000);
             for(int m  = 0; m<10; m++){
                 System.out.println(" ");
